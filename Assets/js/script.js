9acc22b66d440f51c73de9b95d3cc6c17fabc0ce
willsan0723 <william.santee@gmail.com>
2020-09-05 17:27:59 -0600

    added icons and preparing to dynamically change UV index colors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,17 +10,22 @@ function getWeather() {
         return response.json();
       })
       .then(function(data) {
-        var date = (moment().format('M/D/YYYY'));
+        console.log(data.weather[0].icon);
+        var unixTime = data.dt
+        var date = new Date(unixTime * 1000);
+        var newDate = date.toLocaleString();
+        newDate = moment(newDate).format('M/D/YYYY');
+
         var responseContainerEl = document.querySelector('#response-container');
         responseContainerEl.innerHTML = '';
         var cityName = document.getElementById('cityName');
-        cityName.textContent = data.name + '(' + date + ')';
+        cityName.innerHTML = '<h2>' + data.name + '(' + newDate + ')<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png"></img></h2>'
+        
         var current = document.createElement('p');
         current.setAttribute("id", "current");
         current.innerHTML += 'Temperature: ' + data.main.temp + '</br>';
         current.innerHTML += 'Humidity: ' + data.main.humidity +'%</br>';
         current.innerHTML += 'Wind Speed: ' + data.wind.speed + ' MPH</br>';
-
         responseContainerEl.appendChild(current);                
         return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat +
         '&lon=' + data.coord.lon + '&exclude=hourly,minutely&appid=a2ad19dff1334775c857ddd51cc297c4&units=imperial');
@@ -29,15 +34,20 @@ function getWeather() {
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
-            console.log(data.current.uvi);
-            current = document.getElementById("current");
-            current.innerHTML += 'UV Index: ' + data.current.uvi;
 
+            var unixTime = data.current.dt;
+
+            var date = new Date(unixTime * 1000);
+            var newDate = date.toLocaleString();
+            // console.log(newDate);            
+
+            current = document.getElementById("current");
+            current.innerHTML += 'UV Index: <span id="UV">' + data.current.uvi + '</span>';
+            // var newDate = new Date(data.current.dt).toLocaleDateString();
+            // console.log(newDate)
         });
   }
- 
-//UVI
-//onecall
-// uv and 5 day forecast
+  
+// // uv and 5 day forecast &exclude=hourly,minutely
+
 
